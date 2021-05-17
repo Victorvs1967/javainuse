aaa3151fbb537514308842550428a726e0984ed1
Victors1967 <victorsmirnov@me.com>
2021-05-17 17:02:42 +0300

    edit some rows

--- spring-boot-angular/src/main/java/com/vvs/springbootangular/controller/EmplController.java
+++ spring-boot-angular/src/main/java/com/vvs/springbootangular/controller/EmplController.java
@@ -6,18 +6,44 @@ import java.util.List;
 import com.vvs.springbootangular.model.Employee;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
+@RequestMapping("/employees")
 public class EmplController {
   
   private List<Employee> employees = createList();
 
-  @GetMapping("/employees")
+  @GetMapping
   public List<Employee> firstPage() {
     return employees;
+  }
+
+  @DeleteMapping("/{id}")
+  public Employee delete(@PathVariable("id") String id) {
+    Employee deletedEmp = null;
+    for (Employee emp : employees) {
+      if (emp.getId().equals(id)) {
+        employees.remove(emp);
+        deletedEmp = emp;
+        break;
+      }
+    }
+    return deletedEmp;
+  }
+
+  @PostMapping
+  public Employee create(@RequestBody Employee emp) {
+    employees.add(emp);
+    System.out.println(employees);
+    return emp;
   }
 
   private static List<Employee> createList() {
